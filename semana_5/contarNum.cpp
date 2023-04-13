8afc513395aae9621782f7814642d4103bc07aa6
Andre Flores <jeffry.flores@ucsp.edu.pe>
2023-04-13 18:38:33 -0500

    corregido

--- semana_5/contarNum.cpp
+++ semana_5/contarNum.cpp
@@ -5,13 +5,13 @@ int contarNum(long long n){
     int cont = 0;
     while(n > 0){
         cont++;
-        n/10;  
+        n/=10;  
     }
     return cont;
 }
 
 int main(){
-    int a;
+    long long a;
     cin >> a;
     cout << "su numero tiene: " << contarNum(a) << endl;
 }
